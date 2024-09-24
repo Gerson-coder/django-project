069668ecb91d172e86b2ba68cc66e1e0d3c77c12
Gerson <gersonherrerach@gmail.com>
2024-09-23 20:33:17 -0500

    allowed_host

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,7 +122,10 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS =[os.path.join(BASE_DIR, 'blog/static')]
 
+ALLOWED_HOSTS=['*']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
